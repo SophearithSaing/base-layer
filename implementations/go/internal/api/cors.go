0d082386f59413198201c66f09c21dcac18a3d4f
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-06 00:18:07 +0700

    Add preflight handler

--- implementations/go/internal/api/cors.go
+++ implementations/go/internal/api/cors.go
@@ -7,16 +7,24 @@ import (
 
 func Cors(next http.Handler, allowedOrigins []string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-
-		if slices.Contains(allowedOrigins, origin) {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == http.MethodOptions {
+			setCorsHeaders(w, r, allowedOrigins)
+			w.WriteHeader(http.StatusNoContent)
+			return
 		}
-
-		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
+		setCorsHeaders(w, r, allowedOrigins)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func setCorsHeaders(w http.ResponseWriter, r *http.Request, allowedOrigins []string) {
+	origin := r.Header.Get("Origin")
+
+	if slices.Contains(allowedOrigins, origin) {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	}
+
+	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
