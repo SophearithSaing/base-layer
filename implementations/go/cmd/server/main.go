491c91fcadf562e8c0f41b1adb7f64aa96633792
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-07 19:50:19 +0700

    Add separate run function for better error and log handling

--- implementations/go/cmd/server/main.go
+++ implementations/go/cmd/server/main.go
@@ -6,44 +6,48 @@ import (
 	"baselayer/internal/db"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
 func main() {
-	port, err := config.GetPort()
-	if err != nil {
-		fmt.Println(err)
+	if err := run(); err != nil {
+		log.Printf("application error: %v", err)
 		os.Exit(1)
 	}
+}
 
-	mongodbConfig, err := config.GetMongoDBConfig()
+func run() error {
+	port, err := config.GetPort()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("error getting port: %w", err)
 	}
 
 	allowedOrigins, err := config.GetClientOrigins()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("error getting allowed origins: %w", err)
+	}
+
+	mongodbConfig, err := config.GetMongoDBConfig()
+	if err != nil {
+		return fmt.Errorf("error getting mongodb config: %w", err)
 	}
 
 	mongo, err := db.NewMongo(mongodbConfig)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("error initializing mongodb: %w", err)
 	}
 
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		if err := mongo.Close(ctx); err != nil {
-			fmt.Println(err)
+			log.Printf("error closing connection: %v", err)
 		}
 	}()
-	fmt.Println("connected:", mongo.DB.Name())
+	log.Printf("db connected: %v", mongo.DB.Name())
 
 	mux := http.NewServeMux()
 	server := &http.Server{
@@ -55,9 +59,10 @@ func main() {
 
 	api.HandleRoutes(mux)
 
-	fmt.Println("Listening on port " + port)
-	err = server.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
+	log.Printf("listening on port: %v", port)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("server failed: %w", err)
 	}
+
+	return nil
 }
