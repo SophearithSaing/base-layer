4d42cd2977d62165e4747f2f599971767f324a74
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-08 21:59:05 +0700

    Register login route

--- implementations/go/internal/auth/routes.go
+++ implementations/go/internal/auth/routes.go
@@ -6,4 +6,5 @@ import (
 
 func RegisterRoutes(mux *http.ServeMux, handler *Handler) {
 	mux.HandleFunc("POST /auth/register", handler.Register)
+	mux.HandleFunc("POST /auth/login", handler.Login)
 }
