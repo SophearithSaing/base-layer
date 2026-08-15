78c15e9ba5a8859662d7b44731644c6409768bc8
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-15 20:09:50 +0700

    Set cookie based on app env

--- implementations/go/internal/auth/cookie.go
+++ implementations/go/internal/auth/cookie.go
@@ -1,19 +1,26 @@
 package auth
 
 import (
+	"baselayer/internal/config"
 	"net/http"
 	"time"
 )
 
 func setAuthCookie(w http.ResponseWriter, accessToken, refreshToken string) {
 	now := time.Now()
+	isProd := config.GetAppEnv() == "prod"
+	sameSite := http.SameSiteLaxMode
+	if isProd {
+		sameSite = http.SameSiteNoneMode
+	}
+
 	http.SetCookie(w, &http.Cookie{
 		Name:     "access_token",
 		Value:    accessToken,
 		Path:     "/",
 		HttpOnly: true,
-		Secure:   true,
-		SameSite: http.SameSiteNoneMode,
+		Secure:   isProd,
+		SameSite: sameSite,
 		Expires:  now.Add(15 * time.Minute),
 	})
 	http.SetCookie(w, &http.Cookie{
@@ -21,8 +28,8 @@ func setAuthCookie(w http.ResponseWriter, accessToken, refreshToken string) {
 		Value:    refreshToken,
 		Path:     "/auth/refresh",
 		HttpOnly: true,
-		Secure:   true,
-		SameSite: http.SameSiteNoneMode,
+		Secure:   isProd,
+		SameSite: sameSite,
 		Expires:  now.AddDate(0, 0, 30),
 	})
 }
