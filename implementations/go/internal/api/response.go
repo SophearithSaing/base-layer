fe9c4a74a935dec2039601a37c9ea77f99dd627d
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-20 00:24:17 +0700

    Add GenericCreatedResponse

--- implementations/go/internal/api/response.go
+++ implementations/go/internal/api/response.go
@@ -10,6 +10,11 @@ type GenericResponse struct {
 	Message string `json:"message"`
 }
 
+type GenericCreatedResponse struct {
+	Id      string `json:"id"`
+	Message string `json:"message"`
+}
+
 func JSONResponseWriter[T any](w http.ResponseWriter, statusCode int, data T) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
