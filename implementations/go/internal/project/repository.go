6f1fd497224919b9371e2c856a6e910f8be5a8c6
SophearithSaing <sophearithsaing123@gmail.com>
2026-08-19 19:30:54 +0700

    Add project repo and base functions

---
+++ implementations/go/internal/project/repository.go
@@ -0,0 +1,111 @@
+package project
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type Repository struct {
+	ProjectCollection  *mongo.Collection
+	ProgressCollection *mongo.Collection
+}
+
+func NewRepository(db *mongo.Database) *Repository {
+	return &Repository{
+		ProjectCollection:  db.Collection("projects"),
+		ProgressCollection: db.Collection("project_progresses"),
+	}
+}
+
+func (r *Repository) CreateProject(ctx context.Context, project Project) error {
+	_, err := r.ProjectCollection.InsertOne(ctx, project)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) UpdateProject(ctx context.Context, filter bson.M, update bson.M) (Project, error) {
+	var project Project
+	err := r.ProgressCollection.FindOneAndUpdate(ctx, filter, update).Decode(&project)
+	if err != nil {
+		return Project{}, err
+	}
+	return project, nil
+}
+
+func (r *Repository) GetProjectByID(ctx context.Context, id string) (Project, error) {
+	var project Project
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return Project{}, err
+	}
+	filter := bson.D{{Key: "_id", Value: objectID}}
+	err = r.ProjectCollection.FindOne(ctx, filter).Decode(&project)
+	if err != nil {
+		return Project{}, err
+	}
+	return project, nil
+}
+
+func (r *Repository) SearchProjects(ctx context.Context, filter bson.D, sort bson.D) ([]Project, error) {
+	opts := options.Find().SetSort(sort)
+	cursor, err := r.ProgressCollection.Find(ctx, filter, opts)
+	if err != nil {
+		return []Project{}, err
+	}
+	var projects []Project
+	err = cursor.All(ctx, &projects)
+	if err != nil {
+		return []Project{}, err
+	}
+	return projects, nil
+}
+
+func (r *Repository) CreateProgress(ctx context.Context, progress ProjectProgress) error {
+	_, err := r.ProgressCollection.InsertOne(ctx, progress)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) UpdateProgress(ctx context.Context, filter bson.M, update bson.M) error {
+	var progress ProjectProgress
+	err := r.ProgressCollection.FindOneAndUpdate(ctx, filter, update).Decode(&progress)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) GetProgressByID(ctx context.Context, id string) (ProjectProgress, error) {
+	var progress ProjectProgress
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return ProjectProgress{}, err
+	}
+	filter := bson.D{{Key: "_id", Value: objectID}}
+	err = r.ProgressCollection.FindOne(ctx, filter).Decode(&progress)
+	if err != nil {
+		return ProjectProgress{}, err
+	}
+	return progress, nil
+}
+
+func (r *Repository) SearchProgresses(ctx context.Context, filter bson.D, sort bson.D) ([]ProjectProgress, error) {
+	opts := options.Find().SetSort(sort)
+	cursor, err := r.ProgressCollection.Find(ctx, filter, opts)
+	if err != nil {
+		return []ProjectProgress{}, err
+	}
+	var progresses []ProjectProgress
+	err = cursor.All(ctx, &progresses)
+	if err != nil {
+		return []ProjectProgress{}, err
+	}
+	return progresses, nil
+}
